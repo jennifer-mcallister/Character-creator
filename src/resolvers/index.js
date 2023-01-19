861905700cbe55a45075836e0a79f780a5e8e27d
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-19 15:04:18 +0100

    added structure

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,3 +5,30 @@ const { GraphQLError } = require('graphql')
 const crypto = require('crypto')
 
 const charactersDirectory = path.join(__dirname, '..', 'data', 'characters')
+
+exports.resolvers = {
+    Query: {
+        getCharacterByName: async (_, args, context) => {
+
+            return null
+        },
+        getAllCharacters: async (_, args, context) => {
+
+            return null
+        }
+    },
+    Mutation: {
+        createCharacter: async (_, args, context) => {
+
+            return null
+        },
+        updateCharacter: async (_, args, context) => {
+
+            return null
+        },
+        delteCharacter: async (_, args, context) => {
+
+            return null
+        },
+    }
+}
