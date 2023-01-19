b79fae7217fa603d15612fa4c7ef534a027a7a3d
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-01-19 14:54:17 +0100

    added enums for character creating

---
+++ src/enums/charachter.js
@@ -0,0 +1,19 @@
+exports.haircolour = {
+    BLUE: 'BLUE',
+    PINK: 'PINK',
+    GREEN: 'GREEN',
+}
+
+exports.eyecolour = {
+    BLUE: 'BLUE',
+    PINK: 'PINK',
+    GREEN: 'GREEN',
+}
+
+exports.class = {
+    NECROMANCER: 'NECROMANCER',
+    SORCERESS: 'SSORCERESS',
+    ASSASSIN: 'ASSASSIN',
+    BERSERKER: 'BERSERKER',
+
+}
